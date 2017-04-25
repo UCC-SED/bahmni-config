7d46d76c11072a1b3c24ecd61a894d340fa4d727
Ian Manyama <iannashin@gmail.com>
2017-04-25 16:37:21 +0300

    test change procedure

--- openmrs/apps/customDisplayControl/js/customControl.js
+++ openmrs/apps/customDisplayControl/js/customControl.js
@@ -4,7 +4,12 @@ angular.module('bahmni.common.displaycontrol.custom')
     .directive('birthCertificate', ['observationsService', 'appService', 'spinner', function (observationsService, appService, spinner) {
             var link = function ($scope) {
                 console.log("inside birth certificate");
-                var conceptNames = ["HEIGHT", "WEIGHT"];
+                var conceptNames = ["HEIGHT", "Chief Complaint",
+                    "Chief Complaint Notes",
+                    "Lab Samples",
+                    "Coded Diagnosis",
+                    "Diagnosis Certainty",
+                    "Bahmni Diagnosis Status"];
                 $scope.contentUrl = appService.configBaseUrl() + "/customDisplayControl/views/birthCertificate.html";
                 spinner.forPromise(observationsService.fetch($scope.patient.uuid, conceptNames, "latest", undefined, $scope.visitUuid, undefined).then(function (response) {
                     $scope.observations = response.data;
