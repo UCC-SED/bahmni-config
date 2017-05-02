e3433a0d3fbdc28cbc24227fb8e036fbe8b65e7d
Ian Manyama <iannashin@gmail.com>
2017-05-02 12:06:42 +0300

    restore after failed to test filter obs item by patient attributes as gender

--- openmrs/apps/clinical/formConditions.js
+++ openmrs/apps/clinical/formConditions.js
@@ -12,16 +12,16 @@ Bahmni.ConceptSet.FormConditions.rules = {
             }
         }
     },
-    'Systolic Data' : function (formName, formFieldValues, patient) {
-        console.log(patient);
-        if (patient['gender'] == 'Female') {
+    'Systolic Data' : function (formName, formFieldValues) {
+        var systolic = formFieldValues['Systolic'];
+        var diastolic = formFieldValues['Diastolic'];
+        if (systolic || diastolic) {
             return {
                 enable: ["Posture"]
             }
         } else {
             return {
-                disable: ["Posture"],
-                error: "Error message here if any"
+                disable: ["Posture"]
             }
         }
     }
