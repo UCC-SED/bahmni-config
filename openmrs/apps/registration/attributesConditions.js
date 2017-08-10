fac65b7535333129d926d5130a8774d4420405b4
Ian Manyama <iannashin@gmail.com>
2017-08-10 15:56:29 +0300

    Ian Manyama |0000|Remove Pregnancy, Death on Registration

--- openmrs/apps/registration/attributesConditions.js
+++ openmrs/apps/registration/attributesConditions.js
@@ -1,26 +1,3 @@
-var showOrHidePregnancySection = function (patient) {
-    var returnValues = {
-        show: [],
-        hide: []
-    };
-	
-	 console.log(patient);
-
-
-    if (patient["gender"]=="M" ) {
-		 
-        returnValues.hide.push("pregnancyStatus")
-    }else if(patient["age"]["years"]<10)
-	{
-		returnValues.hide.push("pregnancyStatus")
-	}	
-	else {
-		
-        returnValues.show.push("pregnancyStatus")
-    }
-    return returnValues
-};
-
 Bahmni.Registration.AttributesConditions.rules = {
     'gender': function (patient) {
         return showOrHidePregnancySection(patient);
